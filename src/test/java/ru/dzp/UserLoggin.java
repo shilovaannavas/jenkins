f3a715ecae84ba24c4e0191f06d2c75652a37944
Anna Shilova <annashilova@Noutbuk-Anna.local>
2025-11-28 14:39:02 +0300

    first commit

---
+++ src/test/java/ru/dzp/UserLoggin.java
@@ -0,0 +1,36 @@
+package ru.dzp;
+
+import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class UserLoggin {
+
+    @Test
+    void openSite() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium()
+                    .launch(new BrowserType.LaunchOptions().setHeadless(false));
+
+            BrowserContext context = browser.newContext();
+            Page page = context.newPage();
+
+            Allure.step("Открываем сайт", () ->
+                    page.navigate("https://my.dzp-test.devim.team/login")
+            );
+
+            page.waitForLoadState();
+
+            Allure.step("Проверяем текст", () -> {
+                assertThat(page.locator("body")).containsText("Мой текст");
+            });
+
+            Allure.step("Делаем скриншот", () -> {
+                byte[] screenshot = page.screenshot();
+                Allure.addAttachment("Screenshot", "image/png", new java.io.ByteArrayInputStream(screenshot), ".png");
+            });
+        }
+    }
+}
